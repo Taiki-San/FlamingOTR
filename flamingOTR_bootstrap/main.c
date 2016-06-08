c5e243b5c8fa71c052d900b90fe9beb95b9da4e1
Taiki <taiki@rakshata.com>
2016-06-08 17:20:20 +0200

    Add the initial wrapper. You'll need your own Flamingo.app (you can buy it from the MAS) and you own AppIcon (mine was based on the files from the original icon)

---
+++ flamingOTR_bootstrap/main.c
@@ -0,0 +1,50 @@
+//
+//  main.c
+//  flamingOTR_bootstrap
+//
+//  Created by Taiki on 08/06/2016.
+//  Copyright © 2016 Taiki. All rights reserved.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+
+int main(int argc, char const *argv[])
+{
+	char * path;
+	
+	if(argc > 0 && argv[0] != NULL && (path = strdup(argv[0])) != NULL)
+	{
+		unsigned long length = strlen(path);
+		
+		//We are expecting a path looking like /xxx/Contents/MacOS/flamingOTR_bootstrap
+		//We want to remove everything after Contents so we can link to the dylib and the embed binary
+		
+		//Trim the name of the binary from the path
+		while(length > 0 && path[--length] != '/');
+		//Trim the MacOS from the path
+		while(length > 0 && path[--length] != '/');
+		
+		//The path respect our expectation
+		if(length > 0)
+		{
+			//Trim after the point we detected
+			path[length] = 0;
+			
+			char commandLine[length * 2 + 256];
+			
+			//flamingOTR is the main payload which is to be injected in the app embed in the bundle
+			snprintf(commandLine, sizeof(commandLine), "DYLD_INSERT_LIBRARIES=\"%s/Resources/libFlamingOTR.dylib\" %s/Resources/Flamingo.app/Contents/MacOS/Flamingo", path, path);
+			
+			free(path);
+
+			system(commandLine);
+		}
+		else
+			free(path);
+	}
+	
+	return 0;
+}
