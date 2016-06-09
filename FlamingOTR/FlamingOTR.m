a2d81de69c7bfabb9ecf2df45c7130c2d62aa212
Taiki <taiki@rakshata.com>
2016-06-09 18:10:30 +0200

    Add the generic swizzler

--- FlamingOTR/FlamingOTR.m
+++ FlamingOTR/FlamingOTR.m
@@ -8,6 +8,47 @@
 
 #import "FlamingOTR.h"
 
+#include <objc/runtime.h>
+
 @implementation FlamingOTR
+
+//Implementation based on the excellent blog post available at http://nshipster.com/method-swizzling/
+
++ (void) swizzleClass : (Class) class originalMethod : (SEL) originalSelector withMethod : (SEL) swizzledSelector
+{
+	Method originalMethod = class_getInstanceMethod(class, originalSelector);
+	Method swizzledMethod = class_getInstanceMethod(class, swizzledSelector);
+	
+	if(originalMethod != NULL && swizzledMethod != NULL)
+	{
+		BOOL didAddMethod = class_addMethod(class,
+											originalSelector,
+											method_getImplementation(swizzledMethod),
+											method_getTypeEncoding(swizzledMethod));
+		
+		if (didAddMethod)
+		{
+			class_replaceMethod(class,
+								swizzledSelector,
+								method_getImplementation(originalMethod),
+								method_getTypeEncoding(originalMethod));
+		}
+		else
+		{
+			method_exchangeImplementations(originalMethod, swizzledMethod);
+		}
+	}
+	else
+	{
+		if(originalMethod != NULL)
+			NSLog(@"Couldn't find %@ in %@", NSStringFromSelector(swizzledSelector), NSStringFromClass(class));
+
+		else if(swizzledMethod != NULL)
+			NSLog(@"Couldn't find %@ in %@", NSStringFromSelector(originalMethod), NSStringFromClass(class));
+		
+		else
+			NSLog(@"Couldn't find either %@ or %@ in %@", NSStringFromSelector(originalMethod), NSStringFromSelector(swizzledSelector), NSStringFromClass(class))
+	}
+}
 
 @end
