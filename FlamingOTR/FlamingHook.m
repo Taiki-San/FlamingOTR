73a39f2f854baec55d291eb3bfb20fa69c96aa50
Taiki <taiki@rakshata.com>
2016-07-02 22:58:31 +0100

    Add code to handle a scenario where the OTR session wasn't initiated from our side

--- FlamingOTR/FlamingHook.m
+++ FlamingOTR/FlamingHook.m
@@ -65,20 +65,79 @@
 	{
 		if([message.body hasPrefix:@"?OTR"])
 		{
-			FlamingOTRAccount * account = [[FlamingOTR getShared] getContextForSignature:
-										   [FlamingOTRAccount signatureFromMessageTo:message andClient:client]];
+			FlamingOTRAccount * account = [[FlamingOTR getShared] getContextForAccount:[(FGOIMServiceConnection *) client.delegate accountForClient:client]];
+			
 			FlamingOTRSession * session = [account sessionWithUsername:message.to.name];
-			
-			NSString * decryptedMessage = [account decryptMessage:message.body withSession:session];
-			
-			if(decryptedMessage != nil)
-				message.body = decryptedMessage;
-			
-			//Service message, to be discarded
-			else
-				return;
-		}
+			if(session == nil)
+			{
+				//Okay, the ChatViewController doesn't exist.
+				//We'll look if there is a known FGOConversation for this contact
+				NSString * goal = message.from.name;
+				FGOChatListViewController * chatListViewController = ((FGOAppDelegate *) NSApp.delegate).mainWindowController.chatListViewController;
+				__block FGOChatConversation * conversation = nil;
 
+				[chatListViewController.conversations enumerateObjectsUsingBlock:^(FGOChatConversation * obj, NSUInteger idx, BOOL * stop) {
+
+					//FGOChatConversation sadly doesn't contain the email address we need
+					//However, by digging significantly, we could find it, in a NSSet
+					//Our sample only showed one hit but who knows
+					
+					[[obj.handle.handleNames allObjects] enumerateObjectsUsingBlock:^(FGORosterHandleName * obj2, NSUInteger idx2, BOOL * stop2) {
+
+						if([obj2.sanitizedName isEqualToString:goal])
+						{
+							conversation = obj;
+							*stop2 = YES;
+						}
+					}];
+					
+					if(conversation != nil)
+						*stop = YES;
+				}];
+				
+				if(conversation != nil)
+				{
+					FGOChatViewController * controller = [chatListViewController chatViewControllerForConversation:conversation createIfNeeded:YES];
+					
+					//If the ChatViewController was just created, we must push it to the screen for it to populate
+					if([FlamingOTRSession buddyUsernameForController:controller] == nil)
+					{
+						[chatListViewController displayConversationViewController:controller displayType:1];
+					}
+					
+					session = [account sessionWithController:controller];
+				}
+				
+				if(session == nil)
+				{
+					//Ok, this contact isn't in the list of recent contact
+					//We'll send a fake message, wait for the UI to update
+					
+					FGOIMServiceMessage * copyMessage = [message copy];
+					FGOIMServiceConnection * copySelf = (id) self;
+					
+					dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 20 * NSEC_PER_MSEC), dispatch_get_main_queue(), ^{
+						
+						[copySelf client:client didReceiveMessage:copyMessage];
+					});
+					
+					message.body = @"[OTR Service message: Initiating secure chat]";
+				}
+			}
+
+			if(session != nil)
+			{
+				NSString * decryptedMessage = [account decryptMessage:message.body withSession:session];
+				
+				if(decryptedMessage != nil)
+					message.body = decryptedMessage;
+				
+				//Service message, to be discarded
+				else
+					return;
+			}
+		}
+		
 		NSLog(@"Received: %@ to %@ from %@", message.body, message.to.name, message.from.name);
 	}
 	
